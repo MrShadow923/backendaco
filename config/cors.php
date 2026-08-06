1c85a67743efafa2ce3f0db4468502fcb514cdca
Lendon Trenilla <ltrenilla@brokenshire@edu.ph>
2026-08-06 11:09:37 +0800

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   config/cors.php

--- config/cors.php
+++ config/cors.php
@@ -1,26 +1,12 @@
 <?php
-
-return [
-
-    'paths' => [
-        'api/*',
-        'sanctum/csrf-cookie',
-    ],
-
-    'allowed_methods' => ['*'],
-
-    'allowed_origins' => [
-        env('FRONTEND_URL', 'http://localhost:3000'),
-    ],
-
-    'allowed_origins_patterns' => [],
-
-    'allowed_headers' => ['*'],
-
-    'exposed_headers' => [],
-
-    'max_age' => 0,
-
-    'supports_credentials' => true,
-
-];
+ return [
+       'paths' => ['api/*', 'sanctum/csrf-cookie'],
+       'allowed_methods' => ['*'],
+       // Put your exact Vercel URL here:
+       'allowed_origins' => ['https://frontendaco-otug.vercel.app'], 
+       'allowed_origins_patterns' => [],
+       'allowed_headers' => ['*'],
+       'exposed_headers' => [],
+       'max_age' => 0,
+       'supports_credentials' => true, // MUST be true for Sanctum
+   ];
